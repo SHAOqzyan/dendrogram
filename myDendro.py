4d783335c247aa94f6ce2cd81041ffd0d286d14e
yqz206@gmail.com <yqz206@gmail.com>
2019-07-07 16:22:53 +0800

    update

--- myDendro.py
+++ myDendro.py
@@ -16,9 +16,11 @@ class DendroClass:
 
 	dendroData=None
 
+	CO12ResV= 0.158737644553 #km/s
 
 
-
+	catWithLevelTB="cloudCatWithLevel.fit"
+	
 	def __init__(self, CO12FITS ,dendroFITS,regionName): 
 		
 		self.CO12FITS=CO12FITS
@@ -36,8 +38,11 @@ class DendroClass:
 
 		self.dendroCat=self.regionName+"_DendroCat.fit"
 
+		self.maskPath="dendroMask"
 
-	
+		os.system( "mkdir " +self.maskPath )
+		self.maskPath="./dendroMask/"
+
 	def readDendro(self):
 
 		self.dendroData= Dendrogram.load_from(self.dendroFITS )
@@ -77,7 +82,130 @@ class DendroClass:
 		except:
 			pass
 		cat.write(self.dendroCat ,format='fits')
+
+	def headerTo2D(self,head):
+		"""
+		"""
+		newHead=head.copy()
+		del newHead["CRPIX3"]
+		del newHead["CDELT3"]
+		del newHead["CRVAL3"]
+		del newHead["CTYPE3"] 
 		
+		newHead["NAXIS3"]=2
+		newHead["WCSAXES"]=2
+ 
+		return newHead
+
+	def saveMask(self,cloud, FITSData,fitsHeader ):
+		
+		"""
+		save Mask fits 
+		"""
+		cloudID=cloud.idx
+		cloudLevel=cloud.level
+		
+		#saveName=self.dataPath+"dendroCloud{}_level{}.png".format(cloudID,cloudLevel)
+		
+		cloudFeature="Cloud{}_level{}".format(cloudID,cloudLevel)
+		
+		
+		saveFITS=self.maskPath+cloudFeature+"int.fits"
+		saveFITSMask=self.maskPath+cloudFeature+"mask.fits"
+
+
+
+		cloudMask=cloud.get_mask()
+		cloudMaskInt=cloudMask.astype('short') #convert mask to int, essentially 0, 1
+		intMask= np.sum(cloudMaskInt,axis=0) 
+		intMask[intMask>1]=1
+
+		
+		sumXY= np.sum(cloudMaskInt,axis=(1,2)) 
+		
+		nonZeros= np.nonzero(sumXY)[0]
+		
+		first= 	nonZeros[0]
+		second= nonZeros[-1] 
+		
+		
+		cloudCut=FITSData[first:second+1,:,:]
+		#print first, second
+ 
+		intData=np.sum(cloudCut,axis=0)*self.CO12ResV #convert to K km/s 
+		
+		intHead=self.headerTo2D( fitsHeader )
+		
+	 
+		mask_hdu = fits.PrimaryHDU(intMask, intHead)
+
+		data_hdu = fits.PrimaryHDU(intData, intHead)
+		
+		
+		mask_hdu.writeto(saveFITSMask)
+		data_hdu.writeto(saveFITS)
+	
+
+		
+
+	def produceMaskFITS(self):
+		"""
+		
+		produce mask, in 
+		
+		"""
+		#read cat
+		
+		
+		catTB=Table.read(self.dendroCat)
+		
+		newCatTB=catTB.copy()
+		
+		newLevelRow= newCatTB.columns[0].copy()
+		
+		newLevelRow.name="level"
+		
+		newCatTB.add_column(  newLevelRow )
+		
+		
+		
+		fitsHdu=fits.open(searchFITS)[0]
+		
+ 		data=fitsHdu.data
+ 		head=fitsHdu.header
+		
+		indexCol= list( newCatTB["_idx"]  )
+		
+		for eachC in self.dendroData:
+			
+			#tempTB=newCatTB[  newCatTB["_idx"]==eachC.idx  ]
+			
+			rowIndex=indexCol.index(eachC.idx )
+			newCatTB[rowIndex]["level"]=eachC.level
+			
+			statsC =PPVStatistic( eachC )
+			
+			if eachC.level>0 and statsC.area_exact.value<3600:
+				continue # dot con 
+ 	
+			#else write the mask fits
+			
+			#cloudID="Cloud{}".format(eachC.idx)
+			
+			self.saveMask( eachC,data,head)
+		#for eachC in self.dendroData:
+
+ 
+
+		try:
+			os.remove(self.catWithLevelTB)
+		except:
+			pass
+
+		newCatTB.write(self.catWithLevelTB )
+
 doDendro= DendroClass( "G130150merge12.fits", "G130150Dendro.fits","G130150" ) 
 
-doDendro.WriteCatalog()
+#doDendro.WriteCatalog()
+
+doDendro.produceMaskFITS()
